4a2f782fb0f044c8f46ec3e934d1b4d60b1175cc
Cocosaurus <corentin.vilsalmon@gmail.com>
2025-04-17 15:21:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 function updateCountdown() {
-      const today = new Date();
-      const thisYear = today.getFullYear();
-      let birthday = new Date(thisYear, 8, 24); //
+	const today = new Date();
+	const thisYear = today.getFullYear();
+	let birthday = new Date(thisYear, 8, 24); //
 
-      const timeDiff = birthday - today;
-      const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-      document.getElementById('countdown').innerText = `J-${daysLeft}`;
-    }
+	const timeDiff = birthday - today;
+	const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+	document.getElementById('countdown').innerText = `J-${daysLeft}`;
+}
 
-    updateCountdown();
-    // setInterval(updateCountdown, 1000 * 60 * 60); // mise à jour toutes les heures
+updateCountdown();
+
+document.addEventListener("DOMContentLoaded", () => {
+	const TITLES = ["🚨","‼️"];
+
+	let index = 0;
+
+	setInterval(() => {
+		document.title = TITLES[index];
+		index = (index + 1) % titles.length;
+	}, 30000);
+});
+ 

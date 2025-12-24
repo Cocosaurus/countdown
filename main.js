15a34b460228102652f2fdfab6cf561f04647fb8
Corentin Vilsalmon <corentin.vilsalmon@gmail.com>
2025-12-24 20:18:58 +0100

    chore: update countdown and title

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+const titre = document.querySelector("#titre");
+
 function updateCountdown() {
   const today = new Date();
   const thisYear = today.getFullYear();
-  let birthday = new Date(thisYear, 11, 31); //
+  let date = new Date(thisYear, 11, 31); //
 
-  const timeDiff = birthday - today;
+  const timeDiff = date - today;
   const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
   document.getElementById("countdown").innerText = `J-${daysLeft}`;
+
+  // titre.innerText = '' + date;
+  console.log(date);
 }
 
 updateCountdown();

be2d8b96007b396cb49e0eb0b4df276881dd9310
Corentin Vilsalmon <corentin.vilsalmon@gmail.com>
2025-12-24 20:24:42 +0100

    Chore: dynamic title

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ const titre = document.querySelector("#titre");
 function updateCountdown() {
   const today = new Date();
   const thisYear = today.getFullYear();
-  let date = new Date(thisYear, 11, 31); //
+  let date = new Date(thisYear, 11, 31);
+  const dateFormat = date.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric"
+  });
 
   const timeDiff = date - today;
   const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
   document.getElementById("countdown").innerText = `J-${daysLeft}`;
 
-  // titre.innerText = '' + date;
-  console.log(date);
+  titre.innerText = "Jours restants avant le " + dateFormat + " :";
 }
 
 updateCountdown();
